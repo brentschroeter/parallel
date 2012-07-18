d1abe4cf597f77978129c615dd73ef42b6e848ad
Brent Schroeter <narwhaltribe@gmail.com>
2012-07-17 22:53:03 -0700

    remove unnecessary methods

--- test/test_network.py
+++ test/test_network.py
@@ -9,7 +9,7 @@ CONTROL_PORT = 5000
 def main():
 	client = parallel.ParallelClient()
 
-	print 'Enter the command "push" (without quotes) to begin pushing tasks. Enter "kill" to kill the worker and client. Enter "subscribe [IP address]" to begin pulling from a client at the given IP address.'
+	print 'Enter the command "push" (without quotes) to begin pushing tasks. Enter "kill" to kill the worker and client.'
 
 	while True:
 		usr_in = raw_input('> ')
@@ -18,11 +18,6 @@ def main():
 			break
 		elif usr_in == 'push':
 			client.distribute()
-		elif usr_in[:9] == 'subscribe':
-			new_ip = usr_in[10:]
-			vent_addr = 'tcp://%s:%s' % (new_ip, vent_port)
-			sink_addr = 'tcp://%s:%s' % (new_ip, sink_port)
-			client.subscribe(vent_addr, sink_addr)
 		else:
 			print 'Command not recognized.'
 
