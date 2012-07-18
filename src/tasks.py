173ff00c12852497fc9688ac0551bdc35a4651d3
Brent Schroeter <narwhaltribe@gmail.com>
2012-07-17 20:15:51 -0700

    add abstract ImgTask class to potentially reduce bottlenecks

--- src/tasks.py
+++ src/tasks.py
@@ -13,10 +13,23 @@ class Task(object):
 
 class WaitTask(Task):
 	def __init__(self, time=200):
-		super(WaitTask, self).__init__(time + 2000)
+		Task.__init__(self, time + 2000)
 		self.wait_time = time
-		self.id = uuid.uuid4()
 
 	def run(self):
 		time.sleep(float(self.wait_time) * 0.001)
 		return 1
+
+class ImgTask(Task):
+	def __init__(self, img_path, req_timeout=5000):
+		Task.__init__(self, req_timeout)
+		self.img_path = img_path
+
+	def eval_img(self, img_file):
+		raise NotImplementedError
+
+	def run(self):
+		img_file = open(img_path)
+		result_file_path = self.eval_img(img_file)
+		img_file.close()
+		return result_file_path
