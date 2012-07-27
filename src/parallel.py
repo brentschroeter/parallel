4a66a8cae27095e28734a24fee28aa8acf051efe
Brent Schroeter <narwhaltribe@gmail.com>
2012-07-27 12:02:24 -0700

    try to hold pending jobs in queue when all workers are occupied

--- src/parallel.py
+++ src/parallel.py
@@ -13,10 +13,13 @@ VENT_PORT_DEFAULT = '5000'
 SINK_PORT_DEFAULT = '5001'
 
 # push any jobs waiting in the queue
-def send_pending_jobs(queue, sender):
-    while not queue.empty():
-        job_tuple = queue.get()
+def send_pending_jobs(pending_q, sender, sent_jobs, max_sent_jobs):
+    for i in range(max_sent_jobs - len(sent_jobs)):
+        if pending_q.empty():
+            break
+        job_tuple = pending_q.get()
         sender.send(pickle.dumps(job_tuple))
+        sent_jobs.append(job_tuple[2])
 
 # run a job and return the result. assumes that there is a message queued for the receiver.
 def process_job(receiver, sender):
@@ -37,7 +40,7 @@ def add_connection(context, vent_addr, sink_addr):
     return receiver, sender
 
 # push jobs, handle replies, and process incoming jobs
-def worker_loop(context, queue, addresses, callback, vent_port, sink_port):
+def worker_loop(context, pending_q, addresses, callback, vent_port, sink_port):
     vent_sender = context.socket(zmq.PUSH)
     vent_sender.bind('tcp://*:%s' % vent_port)
     time.sleep(0.5)
@@ -50,13 +53,17 @@ def worker_loop(context, queue, addresses, callback, vent_port, sink_port):
         tmp_receiver, tmp_sender = add_connection(context, vent_addr, sink_addr)
         connections.append((tmp_receiver, tmp_sender))
         poller.register(tmp_receiver, zmq.POLLIN)
+    approx_workers = len(connections)
+    sent_jobs = []
     while True:
-        send_pending_jobs(queue, vent_sender)
+        send_pending_jobs(pending_q, vent_sender, sent_jobs, approx_workers)
         while True:
             try:
                 s = sink_receiver.recv(zmq.NOBLOCK)
                 result, job_id = pickle.loads(s)
-                callback(result, job_id)
+                if job_id in sent_jobs:
+                    callback(result, job_id)
+                    sent_jobs.remove(job_id)
             except ZMQError:
                 break
         socks = dict(poller.poll(500))
@@ -70,17 +77,17 @@ def construct_worker(addresses, config={}):
     sink_port = config.get('sink_port', SINK_PORT_DEFAULT)
 
     context = zmq.Context()
-    queue = Queue.Queue()
+    pending_q = Queue.Queue()
 
     def worker(callback):
-        worker_loop(context, queue, addresses, callback, vent_port, sink_port)
+        worker_loop(context, pending_q, addresses, callback, vent_port, sink_port)
 
     def close():
         context.destroy()
 
     def run_job(job, args=()):
         job_id = str(uuid.uuid4())
-        queue.put((job, args, job_id))
+        pending_q.put((job, args, job_id))
         return job_id
 
     return worker, close, run_job
