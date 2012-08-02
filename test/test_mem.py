e5c2b0b3f12f5fe61feb8dd92a0e68834b4047a9
Brent Schroeter <narwhaltribe@gmail.com>
2012-08-01 20:23:59 -0700

    add MEM_LIMIT constant

--- test/test_mem.py
+++ test/test_mem.py
@@ -12,6 +12,7 @@ WORKER_POOL = [('localhost:5000', 'localhost:5001'), ('localhost:5002', 'localho
 # each usage pattern is in the format (sets, sleep time between sets, repetitions per set, sleep time for each job)
 NUM_STRINGS = 500
 STR_LEN = 200
+MEM_LIMIT = 6000000
 
 def str_job(s):
     # reverse the string
@@ -22,17 +23,17 @@ def get_timeout(num_workers):
     computation_time = 5 * NUM_STRINGS / num_workers
     return transportation_time + computation_time
 
-def run_jobs(run_job, fail, size_limit):
+def run_jobs(run_job, fail):
     hp = hpy()
     def generate_str(length):
         return ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(length))
     for i in range(NUM_STRINGS):
         run_job(str_job, (generate_str(STR_LEN)))
     size = hp.heap().size
-    if size > size_limit:
+    if size > MEM_LIMIT:
         fail('Heap size exceeded %d bytes (%d).' % (size_limit, size))
 
-def push(vent_port, sink_port, on_completed, fail, size_limit):
+def push(vent_port, sink_port, on_completed, fail):
     total_completed = [0] # stored as a list as a workaround for Python variable scoping "quirk"
     def result_received(result, job_id):
         total_completed[0] += 1
@@ -40,7 +41,7 @@ def push(vent_port, sink_port, on_completed, fail, size_limit):
             on_completed()
         
     worker, close, run_job = parallel.construct_worker(WORKER_POOL, {'vent_port': vent_port, 'sink_port': sink_port})
-    run_jobs(run_job, fail, size_limit)
+    run_jobs(run_job, fail)
     worker(result_received)
 
 def work(vent_port, sink_port):
@@ -62,7 +63,7 @@ class TestParallel(unittest.TestCase):
                 if completed.value:
                     return True
             return False
-        p1 = multiprocessing.Process(target=push, args=('5000', '5001', on_completed, self.fail, 6000000))
+        p1 = multiprocessing.Process(target=push, args=('5000', '5001', on_completed, self.fail))
         p2 = multiprocessing.Process(target=work, args=('5002', '5003'))
         p3 = multiprocessing.Process(target=work, args=('5004', '5005'))
         p1.start()
