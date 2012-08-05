c766b4dca27e266b55a15cb94fe89d168b7b738b
Brent Schroeter <narwhaltribe@gmail.com>
2012-08-05 11:20:50 -0700

    fix bugs

--- test/test_volume.py
+++ test/test_volume.py
@@ -5,78 +5,81 @@ import unittest
 import multiprocessing
 import time
 import threading
+from collections import namedtuple
+
+UsagePattern = namedtuple('UsagePattern', 'sets set_sleep set_reps job_sleep')
 
 WORKER_POOL = [('localhost:5000', 'localhost:5001'), ('localhost:5002', 'localhost:5003'), ('localhost:5004', 'localhost:5005')]
-# each usage pattern is in the format (sets, sleep time between sets, repetitions per set, sleep time for each job)
-USAGE_PATTERNS = ((5, 5000, 1, 1000), (500, 0, 1, 10), (5, 3000, 100, 100))
+NUM_WORKERS = 2
+USAGE_PATTERNS = (UsagePattern(5, 5000, 1, 1000), UsagePattern(500, 0, 1, 10), UsagePattern(5, 3000, 100, 100))
 
+def construct_worker_pool(num, push_args):
+    processes = []
+    def start(start_port=5000):
+        p = multiprocessing.Process(target=push, args=(start_port, start_port + 1, push_args))
+        p.start()
+        processes.append(p)
+        for i in range(num - 1):
+            start_port += 2
+            p = multiprocessing.Process(target=work, args=(start_port, start_port + 1))
+            p.start()
+            processes.append(p)
+    def kill():
+        for p in processes:
+            p.terminate()
+    return start, kill
 
 def wait_job(ms):
     time.sleep(ms * 0.001)
     return 1
 
-def get_timeout(usage_pattern, num_workers):
-    sets = usage_pattern[0]
-    set_wait = usage_pattern[1]
-    reps = usage_pattern[2]
-    rep_wait = usage_pattern[3]
-    
-    time_per_set = max(set_wait, reps * rep_wait / num_workers)
-    transportation_time = 20 * (sets * reps) + 1000
+def get_timeout(pattern, num_workers):
+    time_per_set = max(pattern.set_sleep, pattern.set_reps * pattern.job_sleep / num_workers)
+    transportation_time = 30 * (pattern.sets * pattern.set_reps) + 1000
 
-    return time_per_set * sets + transportation_time
+    return time_per_set * pattern.sets + transportation_time
 
-def run_jobs_with_pattern(run_job, usage_pattern):
-    for i in range(usage_pattern[0]):
-        time.sleep(usage_pattern[1] * 0.001)
-        for j in range(usage_pattern[2]):
-            run_job(wait_job, (usage_pattern[3]))
+def run_jobs_with_pattern(run_job, pattern):
+    for i in range(pattern.sets):
+        time.sleep(pattern.set_sleep * 0.001)
+        for j in range(pattern.set_reps):
+            run_job(wait_job, (pattern.job_sleep))
 
-def push(vent_port, sink_port, usage_pattern, on_completed):
-    total_jobs = usage_pattern[0] * usage_pattern[2]
-    total_completed = [0] # stored as a list as a workaround for Python variable scoping "quirk"
-    def result_received(result, job_id):
-        total_completed[0] += 1
-        if total_completed[0] == total_jobs:
-            on_completed()
-        
+def push(vent_port, sink_port, args):
+    pattern, result_received = args
     worker, close, run_job = parallel.construct_worker(WORKER_POOL, {'vent_port': vent_port, 'sink_port': sink_port})
-    t = threading.Thread(target=run_jobs_with_pattern, args=[run_job, usage_pattern])
+    t = threading.Thread(target=run_jobs_with_pattern, args=[run_job, pattern])
     t.start()
     worker(result_received)
 
 def work(vent_port, sink_port):
     def result_received(job_id, result):
         pass
-
     worker, close, run_job = parallel.construct_worker(WORKER_POOL, {'vent_port': vent_port, 'sink_port': sink_port})
     worker(result_received)
 
 class TestParallel(unittest.TestCase):
     def test_volume(self):
-        completed = multiprocessing.Value('i')
-        def on_completed():
-            completed.value = True
-        def check_for_completion(timeout):
-            tstart = time.time()
-            while (time.time() - tstart) < timeout * 0.001:
-                if completed.value:
-                    return True
-            return False
-        for usage_pattern in USAGE_PATTERNS:
-            completed.value = False
-            p1 = multiprocessing.Process(target=push, args=('5000', '5001', usage_pattern, on_completed))
-            p2 = multiprocessing.Process(target=work, args=('5002', '5003'))
-            p3 = multiprocessing.Process(target=work, args=('5004', '5005'))
-            p1.start()
-            p2.start()
-            p3.start()
-            timeout = get_timeout(usage_pattern, 3)
-            if not check_for_completion(timeout):
-                self.fail('Failed on usage pattern: %s' % str(usage_pattern))
-            p1.terminate()
-            p2.terminate()
-            p3.terminate()
+        total_completed = multiprocessing.RawValue('i')
+        def result_received(result, job_id):
+            total_completed.value += 1
+        for pattern in USAGE_PATTERNS:
+            def check_for_completion():
+                total_jobs = pattern.sets * pattern.set_reps
+                timeout = get_timeout(pattern, 3)
+                tstart = time.time()
+                while (time.time() - tstart) < timeout * 0.001:
+                    if total_completed.value == total_jobs:
+                        return True
+                return False
+            total_completed.value = 0
+            push_args = (pattern, result_received)
+            start_workers, kill_workers = construct_worker_pool(NUM_WORKERS, push_args)
+            start_workers()
+            if not check_for_completion():
+                self.fail('Failed on usage pattern: %s' % str(pattern))
+            else:
+                kill_workers()
 
 if __name__ == '__main__':
     unittest.main()
