ce723af37e4d2acf74198d221c1658fc725ed6b9
Brent Schroeter <narwhaltribe@gmail.com>
2012-08-20 16:43:38 -0700

    read settings from ec2/config.py

--- ec2/start_instances.py
+++ ec2/start_instances.py
@@ -2,20 +2,11 @@
 
 import boto
 import time
-
-AMI_ID = 'ami-82fa58eb'
-INSTANCE_TYPE = 't1.micro'
-SEC_GROUP = None
+import config
 
 def main():
-    access_key_id = raw_input('Access key ID: ')
-    secret_access_key = raw_input('Secret access key: ')
-    key_name = raw_input('Key name: ')
-    num_instances = int(raw_input('Number of instances: '))
-    SEC_GROUP = raw_input('Security group: ')
-
-    conn = boto.connect_ec2(access_key_id, secret_access_key)
-    conn.run_instances(AMI_ID, min_count=num_instances, max_count=num_instances, key_name=key_name, instance_type=INSTANCE_TYPE, security_groups=[SEC_GROUP])
+    conn = boto.connect_ec2(KEY_ID, SECRET_KEY)
+    conn.run_instances(AMI_ID, min_count=NUM_INSTANCES, max_count=NUM_INSTANCES, key_name=KEYPAIR_NAME, instance_type=INSTANCE_TYPE, security_groups=SEC_GROUPS)
 
     for instance in conn.get_all_instances()[0].instances:
         while not instance.ip_address:
