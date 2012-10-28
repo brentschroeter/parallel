c784997473ba51da25cb4ef612af9072036691d6
Brent Schroeter <narwhaltribe@gmail.com>
2012-10-27 19:17:37 -0700

    add docstrings to test/testing_lib.py

--- test/testing_lib.py
+++ test/testing_lib.py
@@ -9,12 +9,14 @@ import uuid
 TRANSPORT_MS = 50
 
 def worker_addresses(ip_addresses):
+    '''Returns a list of (vent, sink) pairs based on ip_addresses.'''
     worker_addresses = []
     for address in ip_addresses:
         worker_addresses.append(('%s:%s' % (address, parallel.VENT_PORT_DEFAULT), '%s:%s' % (address, parallel.SINK_PORT_DEFAULT)))
     return worker_addresses
 
 def work(vent_port, sink_port, worker_pool):
+    '''Creates one basic worker.'''
     def result_received(job_id, result, args):
         pass
     worker, close, run_job = parallel.construct_worker(worker_pool, {'vent_port': vent_port, 'sink_port': sink_port})
@@ -25,8 +27,8 @@ def pusher(vent_port, sink_port, worker_addresses, send_jobs, sender_args, on_re
     send_jobs(run_job, sender_args)
     worker(on_recv_result, on_recv_result_args)
 
-# provides functions to start a set of workers including 1 pusher and num - 1 normal workers. worker_pool is a list containing vent/sink address pairs.
 def construct_worker_pool(num, worker_addresses, send_jobs, sender_args, on_recv_result, on_recv_result_args, num_pushers=1):
+    '''Constructs functions to start a pool of workers (some of which are also servers).'''
     processes = []
     def start(start_port=5000):
         for i in range(num_pushers):
@@ -44,9 +46,10 @@ def construct_worker_pool(num, worker_addresses, send_jobs, sender_args, on_recv
             p.terminate()
     return start, kill
 
-def check_for_completion(total_completed, num_jobs, timeout):
+def check_for_completion(total_completed, num_jobs, timeout_ms):
+    '''Returns True if multiprocessing.RawValue total_completed is equal to int num_jobs before timeout_ms have passed.'''
     tstart = time.time()
-    while (time.time() - tstart) < timeout * 0.001:
+    while (time.time() - tstart) < timeout_ms * 0.001:
         if total_completed.value == num_jobs:
             return True
     return False
