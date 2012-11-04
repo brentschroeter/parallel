9c295aac9ba24a253018d6bd9c9c11f677fa5476
Brent Schroeter <narwhaltribe@gmail.com>
2012-11-04 10:01:38 -0800

    add docstring to main() of test/worker.py.

--- test/worker.py
+++ test/worker.py
@@ -5,6 +5,10 @@ import testing_lib
 import sys
 
 def main():
+    '''
+    Runs creates and runs one worker.
+    example usage: ./worker.py localhost 10.0.1.3 10.0.1.4
+    '''
     worker_addresses = testing_lib.worker_addresses(sys.argv[1:])
     worker, close, run_job = parallel.construct_worker(worker_addresses)
     print 'Worker running with default ports...'
